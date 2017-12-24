f8461b2f73c75be5cb8a90ac7e74ff705f42931a
Santosh <g.santosh.sunny@gmail.com>
2017-12-24 21:08:16 +0530

    Update script.js
    chatbox

--- javascripts/script.js
+++ javascripts/script.js
@@ -58,5 +58,17 @@ $(function(){
 
     
   
+     // <!--Start of Tawk.to Script-->
+     var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+     (function(){
+     var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+     s1.async=true;
+     s1.src='https://embed.tawk.to/5a3fbcd2bbdfe97b137fd1f3/default';
+     s1.charset='UTF-8';
+     s1.setAttribute('crossorigin','*');
+     s0.parentNode.insertBefore(s1,s0);
+     })();
+     // <!--End of Tawk.to Script-->
+  
   
 });
